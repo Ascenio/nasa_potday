073075a5ebd6a2b1cb794012ec5961ed111199ce
Ascênio Sanderson <ascenio@hotmail.com>
2024-05-06 16:32:01 -0300

    feat: show title and date on listing of pictures

--- lib/features/picture_of_the_day/presentation/widgets/picture_widget.dart
+++ lib/features/picture_of_the_day/presentation/widgets/picture_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nasa_potday/features/picture_of_the_day/domain/entities/picture_entity.dart';
 import 'package:nasa_potday/features/picture_of_the_day/presentation/pages/details_page.dart';
+import 'package:nasa_potday/features/picture_of_the_day/presentation/widgets/date_widget.dart';
 
 class PictureWidget extends StatelessWidget {
   const PictureWidget({
@@ -18,18 +19,37 @@ class PictureWidget extends StatelessWidget {
           MaterialPageRoute(builder: (_) => DetailsPage(picture: picture)),
         );
       },
-      child: Hero(
-        tag: picture.url,
-        child: switch (picture.isVideo) {
-          true => const Placeholder(
-              child: SizedBox.expand(
-                child: Center(child: Text('Video is not implemented yet')),
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Hero(
+            tag: picture.url,
+            child: switch (picture.isVideo) {
+              true => const Placeholder(
+                  child: SizedBox.expand(
+                    child: Center(child: Text('Video is not implemented yet')),
+                  ),
+                ),
+              false => Image.network(
+                  picture.url.toString(),
+                ),
+            },
+          ),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
             ),
-          false => Image.network(
-              picture.url.toString(),
+            child: Text(picture.title),
+          ),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
             ),
-        },
+            child: DateWidget(date: picture.date),
+          ),
+        ],
       ),
     );
   }
